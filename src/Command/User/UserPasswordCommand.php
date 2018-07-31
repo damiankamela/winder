4c68515ea8c6b2eb11f7a99539b76943a7167f0c
Damian Kamela <damian.kamela@dige.pl>
2018-07-31 21:23:31 +0200

    change user password command

--- src/Command/User/UserPasswordCommand.php
+++ src/Command/User/UserPasswordCommand.php
@@ -4,23 +4,19 @@ declare(strict_types=1);
 
 namespace App\Command\User;
 
-use App\Entity\Admin;
-use App\Entity\Employee;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Validator\ConstraintViolationListInterface;
 
-class CreateUserCommand extends ContainerAwareCommand
+class UserPasswordCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
         $this
-            ->setName('app:user:create')
-            ->setDescription('Creates user.');
+            ->setName('app:user:change-password')
+            ->setDescription('Changes user password.');
     }
 
     /**
@@ -31,22 +27,12 @@ class CreateUserCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln([
-            'User Creator',
+            'User Password Changer',
             '============',
             '',
         ]);
 
         $helper = $this->getHelper('question');
-
-        $question = new ChoiceQuestion(
-            'Please select user type:',
-            ['employee', 'admin'],
-            0
-        );
-
-        $question->setErrorMessage('User type %s is invalid.');
-
-        $type = $helper->ask($input, $output, $question);
 
         $question = new Question('Username: ');
         $username = $helper->ask($input, $output, $question);
@@ -54,8 +40,8 @@ class CreateUserCommand extends ContainerAwareCommand
         $userRepository = $this->getContainer()->get('doctrine')->getRepository(User::class);
         $user = $userRepository->findOneBy(compact('username'));
 
-        if ($user) {
-            $output->writeln(sprintf('User %s already exists.', $username));
+        if (!$user) {
+            $output->writeln(sprintf('User %s not found.', $username));
             return;
         }
 
@@ -63,19 +49,11 @@ class CreateUserCommand extends ContainerAwareCommand
         $question->setHidden(true);
         $password = $helper->ask($input, $output, $question);
 
-        $class = $type === 'employee' ? Employee::class : Admin::class;
-
-        /** @var User $user */
-        $user = new $class;
-
-        $user->setUsername($username);
         $user->setPlainPassword($password);
 
         $em = $this->getContainer()->get('doctrine')->getManager();
-
-        $em->persist($user);
         $em->flush();
 
-        $output->writeln(sprintf('User %s created successfully.', $username));
+        $output->writeln('Password changed successfully.');
     }
 }
